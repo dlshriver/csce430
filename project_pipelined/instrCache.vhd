4bdaebd80dd54c54f0e026ea10c3ed9044a83cf3
David Shriver <dshriver@CSELAB4-09.cs.unl.edu>
2015-04-24 11:20:30 -0500

    OMG! real(er) caches

--- project_pipelined/instrCache.vhd
+++ project_pipelined/instrCache.vhd
@@ -19,6 +19,7 @@ Architecture behavior of instrCache is
 	shared variable missCount : integer := 0;
 	shared variable stallCount : integer := 0;
 	shared variable prevAddr : std_logic_vector(15 downto 0) := x"FFFF";
+	signal setEntry : integer := 0;
 	
 	type mem_t is array(0 to 255) of std_logic_vector(23 downto 0);
 	signal ram : mem_t;
@@ -26,10 +27,13 @@ Architecture behavior of instrCache is
 	attribute ram_init_file of ram :
 	signal is "instr.mif";
 	
-	type set is array(0 to 127) of std_logic_vector(23 downto 0);
-	type iCache is array(0 to 1) of set;
-	signal set1 : set := (others => x"000000");
-	signal set2 : set := (others => x"000000");
+	type entry is array(0 to 127) of std_logic_vector(23 downto 0);
+	type meta is array(0 to 1) of std_logic_vector(1 downto 0);
+	type set is array(0 to 1) of entry;
+	signal entry1 : entry := (others => x"000000");
+	signal entry2 : entry := (others => x"000000");
+	signal set1 : set := (entry1, entry2);
+	signal set1Meta : meta := (others => "00");
 Begin
 	controller : process (clock, reset) is
 	begin
@@ -49,20 +53,23 @@ Begin
 				end if;
 				prevAddr := address;
 				hit <= std_logic_vector(to_unsigned(hitCount, 16));
-				if address(7) = '0' then
-					data <= set1(conv_integer(address(6 downto 0)));
-				else
-					data <= set2(conv_integer(address(6 downto 0)));
-				end if;
+				for i in 0 to 1 loop
+					if (set1Meta(i)(0) = address(7) and set1Meta(i)(1) = '1') then
+						setEntry <= i;
+					end if;
+				end loop;
+				data <= set1(setEntry)(conv_integer(address(6 downto 0)));
 				cacheStall <= '0';
 			else
 				cacheStall <= '1';
 				if (stallCount = 256) then
+					set1Meta(0) <= "10";
+					set1Meta(1) <= "00";
 					prefetch := '1';
 					missCount := missCount + 1;
 					miss <= std_logic_vector(to_unsigned(missCount, 16));
 				elsif (stallCount < 128) then
-					set1(stallCount) <= ram(stallCount);
+					set1(0)(stallCount) <= ram(stallCount);
 				end if;
 				stallCount := stallCount + 1;
 			end if;
